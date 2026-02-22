3773b0fa4777d3ffa350ba2fefbebd8b0a1698cf
Waleed-070 <w56772486@gmail.com>
2026-02-23 00:01:37 +0500

    chore: raise chunkSizeWarningLimit to 2000 for Three.js/R3F

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Three.js + R3F are large; suppress false-positive warning
+  },
 })
